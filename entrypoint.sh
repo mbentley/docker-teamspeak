6da2ca9d8f9f3d9d82c5305b0d2768186b328cdd
Matt Bentley <mbentley@mbentley.net>
2017-07-31 21:35:42 -0400

    Cleanup entrypoint shell script
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
 function stop {
-	kill $(ps -C ts3server -o pid= | awk '{ print $1; }')
-	exit
+  kill "$(ps -C ts3server -o pid= | awk '{ print $1; }')"
+  exit
 }
 
 trap stop INT
@@ -17,28 +17,28 @@ test -d /data/files || mkdir -p /data/files && chown teamspeak:teamspeak /data/f
 test -d /data/logs || mkdir -p /data/logs && chown teamspeak:teamspeak /data/logs
 
 # create symlinks for all files and directories in the persistent data directory
-cd ${TS_DIRECTORY}
-for i in $(ls /data)
+cd "${TS_DIRECTORY}"
+for i in /data/*
 do
-	ln -sf /data/${i}
+  ln -sf "${i}"
 done
 
 # remove broken symlinks
-find -L ${TS_DIRECTORY} -type l -delete
+find -L "${TS_DIRECTORY}" -type l -delete
 
 # create symlinks for static files
 STATIC_FILES=(
-	query_ip_whitelist.txt
-	query_ip_blacklist.txt
-	ts3server.ini
-	ts3server.sqlitedb
-	ts3server.sqlitedb-shm
-	ts3server.sqlitedb-wal
+  query_ip_whitelist.txt
+  query_ip_blacklist.txt
+  ts3server.ini
+  ts3server.sqlitedb
+  ts3server.sqlitedb-shm
+  ts3server.sqlitedb-wal
 )
-for i in ${STATIC_FILES[@]}
+for i in "${STATIC_FILES[@]}"
 do
-	ln -sf /data/${i}
+  ln -sf /data/"${i}"
 done
 
 export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
-exec /tini -- ./ts3server $@
+exec /tini -- ./ts3server "$@"
