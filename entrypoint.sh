7768e9fbc6683eb00bde284fb73e877ee8ad6738
Matt Bentley <mbentley@mbentley.net>
2018-05-03 07:41:26 -0400

    Size improvements and entrypoint fixes
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>
    
    - Added alpine linux as primary as 37 MB is better than 179MB
    - Changed entrypoint to use sh; removed code that didn't work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
-
-function stop {
-  kill "$(ps -C ts3server -o pid= | awk '{ print $1; }')"
-  exit
-}
-
-trap stop INT
-trap stop TERM
 
 # create directory for teamspeak files
 test -d /data/files || mkdir -p /data/files && chown teamspeak:teamspeak /data/files
@@ -27,28 +19,21 @@ done
 find -L "${TS_DIRECTORY}" -type l -delete
 
 # create symlinks for static files
-STATIC_FILES=(
-  query_ip_whitelist.txt
-  query_ip_blacklist.txt
-  ts3server.ini
-  ts3server.sqlitedb
-  ts3server.sqlitedb-shm
-  ts3server.sqlitedb-wal
-  .ts3server_license_accepted
-)
-for i in "${STATIC_FILES[@]}"
+STATIC_FILES="query_ip_whitelist.txt query_ip_blacklist.txt ts3server.ini ts3server.sqlitedb ts3server.sqlitedb-shm ts3server.sqlitedb-wal .ts3server_license_accepted"
+
+for i in ${STATIC_FILES}
 do
   ln -sf /data/"${i}"
 done
 
 # check to see if license agreement method has been passed (this doesn't validate the license agreement acceptance; just a basic check)
-if [ -f "${TS_DIRECTORY}/.ts3server_license_accepted" ] || [[ "$*" = *"license_accepted=1"* ]] || [ "${TS3SERVER_LICENSE}" = "accept" ]
+if [ -f "${TS_DIRECTORY}/.ts3server_license_accepted" ] || [ "$(echo "$*" | grep -q "license_accepted=1"; echo $?)" = "0" ] || [ "${TS3SERVER_LICENSE}" = "accept" ]
 then
   echo "Found a license agreement method; launching TeamSpeak"
 else
   echo "Warning: license agreement method hasn't been passed; see the README (https://github.com/mbentley/docker-teamspeak#license-agreement) for how to do so with this Docker image"
-  echo -e "Note: if you're running TeamSpeak < 3.1.0; you can safely ignore this message\\n"
+  echo "Note: if you're running TeamSpeak < 3.1.0; you can safely ignore this message"; echo
 fi
 
 export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
-exec /tini -- ./ts3server "$@"
+exec /sbin/tini -- ./ts3server "$@"
